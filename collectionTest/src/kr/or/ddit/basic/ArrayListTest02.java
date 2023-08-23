cecb7f9401da7651285b7c078b07f6d4a8bebf8e
sohyunj67 <PC-03@DESKTOP-920LC6B>
2023-08-23 14:53:25 +0900

    수업

--- collectionTest/src/kr/or/ddit/basic/ArrayListTest02.java
+++ collectionTest/src/kr/or/ddit/basic/ArrayListTest02.java
@@ -12,51 +12,54 @@ import java.util.Scanner;
 public class ArrayListTest02 {
 
 	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<String>();
+		ArrayList<String> nameList = new ArrayList<String>();
 		Scanner sc = new Scanner(System.in);
 
 		for (int i = 0; i < 5; i++) {
-			System.out.print(i + "번째 이름 입력 >> ");
-			list.add(sc.nextLine());
+			System.out.print((i + 1) + "번째 이름 입력 >> ");
+			nameList.add(sc.nextLine());
 		}
 
 		System.out.println();
 
-		// 방법1
 		System.out.println("---- 방법1 ----");
-		for (String str : list)
+		for (int i = 0; i < nameList.size(); i++) {
+			if (nameList.get(i).substring(0, 1).equals("김")) {
+				System.out.println(nameList.get(i));
+			}
+		}
+
+		System.out.println("---- 방법2 ----");
+		for (int i = 0; i < nameList.size(); i++) {
+			if (nameList.get(i).startsWith("김")) {
+				System.out.println(nameList.get(i));
+			}
+		}
+
+		System.out.println("---- 방법3 ----");
+		for (String str : nameList)
 			if (str.indexOf("김") == 0) {
 				System.out.println(str);
 			}
 
-		// 방법2
-		System.out.println("---- 방법2 ----");
-		for (String str : list) {
+		System.out.println("---- 방법4 ----");
+		for (String str : nameList) {
 			if (str.charAt(0) == '김') {
 				System.out.println(str);
 			}
 		}
 
-		// 방법3
-		System.out.println("---- 방법3 ----");
-		for (String str : list) {
+		// 밑에 두개는 수업때 안함
+		System.out.println("---- 방법5 ----");
+		for (String str : nameList) {
 			if (str.contains("김")) {
 				System.out.println(str);
 			}
 		}
 
-		// 방법4
-		System.out.println("---- 방법4 ----");
-		for (String str : list) {
+		System.out.println("---- 방법6 ----");
+		for (String str : nameList) {
 			if (str.matches("김.*")) {
-				System.out.println(str);
-			}
-		}
-
-		// 방법5
-		System.out.println("---- 방법5 ----");
-		for (String str : list) {
-			if (str.substring(0, 1).equals("김")) { //str.substring(0,1)=="김" 주소값 비교라 하면 안됨
 				System.out.println(str);
 			}
 		}
