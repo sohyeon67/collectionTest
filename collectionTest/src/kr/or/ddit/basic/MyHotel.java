b17951c89d9396e9951138677594709535917a8e
sohyunj67 <PC-03@DESKTOP-920LC6B>
2023-08-29 15:14:21 +0900

    commit

--- collectionTest/src/kr/or/ddit/basic/MyHotel.java
+++ collectionTest/src/kr/or/ddit/basic/MyHotel.java
@@ -223,43 +223,50 @@ public class MyHotel {
 	public MyHotel() {
 		hotelMap = new HashMap<Integer, Room>();
 		scan = new Scanner(System.in);
-
-		for (int i = 201; i <= 209; i++) {
-			hotelMap.put(i, new Room(i, "싱글룸"));
+		
+		// 객실 초기화
+		for(int i=2; i<=4; i++) {
+			// 방 종류 구하기
+			String roomType = null;
+			switch(i) {
+			case 2: roomType = "싱글룸"; break;
+			case 3: roomType = "더블룸"; break;
+			case 4: roomType = "스위트룸"; break;
+			}
+			
+			// 각 객실(Room객체)을 생성해서 Map에 저장한다.
+			for(int j=1; j<=9; j++) {
+				int roomNum = i * 100 + j;
+				Room room = new Room(roomNum, roomType);
+				hotelMap.put(roomNum, room);
+			}
 		}
-
-		for (int i = 301; i <= 309; i++) {
-			hotelMap.put(i, new Room(i, "더블룸"));
-		}
-
-		for (int i = 401; i <= 409; i++) {
-			hotelMap.put(i, new Room(i, "스위트룸"));
-		}
-	}
+	} // 생성자 끝...
 
 	public static void main(String[] args) {
-		new MyHotel().startMyHotel();
+		new MyHotel().hotelStart();
 	}
 
-	public void startMyHotel() {
+	// 시작 메서드
+	public void hotelStart() {
 		System.out.println("*********************************************");
-		System.out.println("       호텔문을 열었습니다. 어서오십시요.");
+		System.out.println("             호 텔 영 업 관 리 프 로 그 램");
 		System.out.println("*********************************************");
 
 		while (true) {
 			int choice = displayMenu();
 
 			switch (choice) {
-			case 1:
+			case 1:		// 체크인
 				checkIn();
 				break;
-			case 2:
+			case 2:		// 체크아웃
 				checkOut();
 				break;
-			case 3:
-				displayState();
+			case 3:		// 객실상태
+				displayRoomState();
 				break;
-			case 4:
+			case 4:		// 업무종료
 				System.out.println();
 				System.out.println("*********************************************");
 				System.out.println("       호텔문을 닫았습니다.");
@@ -267,11 +274,13 @@ public class MyHotel {
 				return;
 			default:
 				System.out.println("작업 번호를 잘못 입력했습니다...");
+				System.out.println("다시 입력하세요...");
 			}
 		}
 
 	}
 
+	// 체크인 처리 메서드
 	private void checkIn() {
 		System.out.println();
 		System.out.println("----------------------------------------------");
@@ -284,89 +293,84 @@ public class MyHotel {
 		System.out.print("방 번호 입력 >> ");
 
 		int roomNum = scan.nextInt();
-
-		String roomType;
-		if (roomNum >= 201 && roomNum <= 209) {
-			roomType = "싱글룸";
-		} else if (roomNum >= 301 && roomNum <= 309) {
-			roomType = "더블룸";
-		} else if (roomNum >= 401 && roomNum <= 409) {
-			roomType = "스위트룸";
-		} else {
+		
+		// 입력한 값이 Map의 key값에 없으면 존재하지 않는 방번호 이다.
+		if(!hotelMap.containsKey(roomNum)) {
 			System.out.println(roomNum + "호 객실은 존재하지 않습니다.");
 			return;
 		}
 
-		if (!hotelMap.get(roomNum).getName().equals("-")) {
+		// 해당 객실에 투숙객이 있는지 검사
+		if (hotelMap.get(roomNum).getName()!=null) {
 			System.out.println(roomNum + "호 객실은 이미 손님이 있습니다.");
 			return;
 		}
 
 		System.out.println("누구를 체크인 하시겠습니까?");
 		System.out.print("이름 입력 >> ");
-		String name = scan.next();
-		Room room = new Room(roomNum, roomType);
-		room.setName(name);
-		hotelMap.put(roomNum, room);
+		String guestName = scan.next();
+		
+		// 입력 받은 투숙객 이름을 해당 객실의 투숙객 이름으로 저장한다.
+		hotelMap.get(roomNum).setName(guestName);
 
-		System.out.println("체크인이 완료되었습니다.");
+		System.out.println(guestName + "씨가 "+ roomNum + "호 객실에 체크인이 되었습니다...");
 	}
 
-	private void displayState() {
+	// 객실 상태를 출력하는 메서드
+	private void displayRoomState() {
 		System.out.println();
-		System.out.println("----------------------------------------------");
+		System.out.println("----------------------------------");
 		System.out.println("		현재 객실 상태");
-		System.out.println("----------------------------------------------");
+		System.out.println("----------------------------------");
 		System.out.println("방 번호	 방 종류	 투숙객 이름");
-		System.out.println("----------------------------------------------");
+		System.out.println("----------------------------------");
 
-		Set<Integer> hotelSet = hotelMap.keySet();
+		Set<Integer> roomNumSet = hotelMap.keySet();
+		ArrayList<Integer> roomNumList = new ArrayList<Integer>(roomNumSet);
+		Collections.sort(roomNumList);	// 방번호를 오름차순으로 정렬
 		
-		ArrayList<Integer> hotelList = new ArrayList<Integer>(hotelSet);
-		
-		Collections.sort(hotelList);
-		for(int key : hotelList) {
-			Room r = hotelMap.get(key);
-			System.out.println(r.getRoomNum() + "\t" + r.getRoomType() + "\t" + r.getName());
+		for(int roomNum : roomNumList) {
+			Room r = hotelMap.get(roomNum);
+			System.out.print(r.getNum() + "\t");
+			System.out.print(r.getType() + "\t");
+			String name = r.getName()==null ? "-" : r.getName();
+			System.out.println(name);
 		}
 	}
 
+	// 체크아웃을 처리하는 메서드
 	private void checkOut() {
 		System.out.println();
-		System.out.println("----------------------------------------------");
+		System.out.println("----------------------------------");
 		System.out.println("   체크아웃 작업");
-		System.out.println("----------------------------------------------");
+		System.out.println("----------------------------------");
 
 		System.out.println("체크아웃 할 방 번호를 입력하세요.");
 		System.out.print("방번호 입력 >> ");
 		int roomNum = scan.nextInt();
 
-		
-		String roomType;
-		if (roomNum >= 201 && roomNum <= 209) {
-			roomType = "싱글룸";
-		} else if (roomNum >= 301 && roomNum <= 309) {
-			roomType = "더블룸";
-		} else if (roomNum >= 401 && roomNum <= 409) {
-			roomType = "스위트룸";
-		} else {
+		// 입력한 값이 Map의 key값에 없으면 존재하지 않는 방번호 이다.
+		if(!hotelMap.containsKey(roomNum)) {
 			System.out.println(roomNum + "호 객실은 존재하지 않습니다.");
 			return;
 		}
-		
-		String name = hotelMap.get(roomNum).getName();
-		
-		if (name.equals("-")) {
-			System.out.println(roomNum + "호 객실에는 체크인 한 사람이 없습니다.");
+
+		// 해당 객실에 투숙객이 없는지 검사
+		if (hotelMap.get(roomNum).getName()==null) {
+			System.out.println(roomNum + "호 객실에는 체크인 한 손님이 없습니다.");
 			return;
 		}
 		
-		hotelMap.get(roomNum).setName("-");	// remove쓰면 안됨
-		System.out.println(roomNum + "호 객실의 " + name + "님 체크아웃을 완료하였습니다.");
+		// 해당 객실에 현재 투숙 중인 투숙객 이름 구하기
+		String guestName = hotelMap.get(roomNum).getName();
 		
+		// 체크 아웃 작업은 해당 객실의 투숙객 이름을 null로 변경한다.
+		hotelMap.get(roomNum).setName(null);	// remove쓰면 안됨
 		
+		System.out.println(roomNum + "호 객실의 " + guestName + "님 체크아웃을 완료하였습니다.");
 	}
 
+	// 메뉴를 출력하고 선택 번호를 입력받아 반환하는 메서드
 	private int displayMenu() {
 		System.out.println();
 		System.out.println("-----------------------------------------------------------");
@@ -381,31 +385,30 @@ public class MyHotel {
 }
 
 class Room {
-	private int roomNum;
-	private String roomType;
-	private String name;
-
-	public Room(int roomNum, String roomType) {
+	private int num;		// 방번호
+	private String type;	// 방종류
+	private String name;	// 투숙객 이름
+	
+	public Room(int num, String type) {
 		super();
-		this.roomNum = roomNum;
-		this.roomType = roomType;
-		name = "-";
+		this.num = num;
+		this.type = type;
 	}
 
-	public int getRoomNum() {
-		return roomNum;
+	public int getNum() {
+		return num;
 	}
 
-	public void setRoomNum(int roomNum) {
-		this.roomNum = roomNum;
+	public void setNum(int num) {
+		this.num = num;
 	}
 
-	public String getRoomType() {
-		return roomType;
+	public String getType() {
+		return type;
 	}
 
-	public void setRoomType(String roomType) {
-		this.roomType = roomType;
+	public void setType(String type) {
+		this.type = type;
 	}
 
 	public String getName() {
