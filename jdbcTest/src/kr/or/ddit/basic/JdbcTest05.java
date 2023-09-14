4e145b981f1e57d172408592f9cf6648153e4509
sohyunj67 <PC-03@DESKTOP-920LC6B>
2023-09-14 16:54:17 +0900

    commit

--- jdbcTest/src/kr/or/ddit/basic/JdbcTest05.java
+++ jdbcTest/src/kr/or/ddit/basic/JdbcTest05.java
@@ -8,13 +8,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
+import kr.or.ddit.util.DBUtil;
+
 /*
 	LPROD테이블에 새로운 데이터를 추가한다.
 	
 	조건) lprod_gu값과 lprod_nm값은 직접 입력 받아서 처리하고,
 		lprod_id값은 현재의 lprod_id값 중에서 제일 큰 값보다 1 크게 한다.
 		입력 받은 lprod_gu값이 이미 등록되어 있으면 다시 입력 받아 처리한다.
-
 */
 
 public class JdbcTest05 {
@@ -23,69 +24,87 @@ public class JdbcTest05 {
 		Scanner scan = new Scanner(System.in);
 		
 		Connection conn = null;
-		Statement stmt = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		
-		int lprodId = 0;
-		String lprodGu = null;
-		String lprodNm = null;
-		
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection(
-					"jdbc:oracle:thin:@localhost:1521:xe", "pc03", "java");
+//			Class.forName("oracle.jdbc.driver.OracleDriver");
+//			
+//			conn = DriverManager.getConnection(
+//					"jdbc:oracle:thin:@localhost:1521:xe", "pc03", "java");
 			
-			System.out.println("=== 새로운 데이터 추가하기 ===");
-			while(true) {
-				System.out.print("lprod_gu : ");
-				lprodGu = scan.next().toUpperCase();
-				
-				String checkSql = "select count(*) from lprod "
-						+ "where lprod_gu='" + lprodGu + "'";
-				
-				stmt = conn.createStatement();
-				rs = stmt.executeQuery(checkSql);
-				if(rs.next()) {
-					if(rs.getInt(1) == 0) {
-						break;
-					}
-					System.out.println("이미 존재하는 lprod_gu값 입니다.");
-					System.out.println("다시 입력해주세요...");
-				}
-			}
-			System.out.print("lprod_nm : ");
-			lprodNm = scan.next();
+			conn = DBUtil.getConnection();
 			
-			String maxSql = "select max(lprod_id) from lprod";
-			rs = stmt.executeQuery(maxSql);
-			if(rs.next()) {
-				lprodId = rs.getInt(1)+1;
-			}
-			
-			String sql = "insert into lprod (lprod_id, lprod_gu, lprod_nm) "
-					+ " values( ?, ?, ?)";
-			
+			// lprod_id값 구하기  ==> 현재의 lprod_id값 중 제일 큰 값보다 1 큰 값으로 한다.
+			String sql = "select nvl(max(lprod_id),0) maxnum from lprod";
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, lprodId);
-			pstmt.setString(2, lprodGu);
-			pstmt.setString(3, lprodNm);
+			
+			rs = pstmt.executeQuery();
+			int maxnum = 0;
+			if(rs.next()) {
+				maxnum = rs.getInt(1);			// 컬럼 번호 이용
+//				maxnum = rs.getInt("maxnum");	// alias명 이용
+			}
+			maxnum++;	// 제일 큰 값 증가하기
+			//------------------------------------------------
+			
+			// 입력 받은 lprod_gu값이 이미 등록되어 있으면 다시 입력 받아 처리한다.
+			String gu;		// 상품 분류 코드가 저장될 변수 선언
+			int count = 0;	// 검색한 상품 분류 코드의 개수가 저장될 변수 선언
+			
+			String sql2 = "select count(*) cnt from lprod where lprod_gu = ?";
+			pstmt = conn.prepareStatement(sql2);
+			do {
+				System.out.print("상품 분류 코드(LPROD_GU) 입력 >> ");
+				gu = scan.next().toUpperCase();
+				
+				pstmt.setString(1, gu);
+				
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					count = rs.getInt("cnt");
+				}
+				
+				if(count>0) {
+					System.out.println("입력한 상품 분류 코드 " + gu +
+							"는(은) 이미 등록된 코드입니다.");
+					System.out.println("다른 코드로 다시 입력하세요...");
+					System.out.println();
+				}
+			} while(count>0);
+			
+			//------------------------------------------------
+			
+			System.out.print("상품 분류명(LPROD_NM) 입력 >> ");
+			String nm = scan.next();
+			
+			//------------------------------------------------
+			
+			String sql3 = "insert into lprod (lprod_id, lprod_gu, lprod_nm) "
+					+ " values(?, ?, ?)";
+			pstmt = conn.prepareStatement(sql3);
+			pstmt.setInt(1, maxnum);
+			pstmt.setString(2, gu);
+			pstmt.setString(3, nm);
 			
 			int cnt = pstmt.executeUpdate();
-			if(cnt == 1) System.out.println("데이터 추가 성공!!");
+			if(cnt>0) {
+				System.out.println("insert 성공!!!");
+			} else {
+				System.out.println("insert 실패~~~");
+			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
 		} finally {
 			if(rs!=null) try { rs.close(); } catch(SQLException e) {}
-			if(stmt!=null) try { stmt.close(); } catch(SQLException e) {}
-			if(pstmt!=null) try {pstmt.close();}catch(SQLException e) {}
+			if(pstmt!=null) try { pstmt.close(); } catch(SQLException e) {}
 			if(conn!=null) try { conn.close(); } catch(SQLException e) {}
 		}
 		
-		
 	}
-
+	
 }
---
+++ jdbcTest/src/kr/or/ddit/basic/JdbcTest06.java
@@ -0,0 +1,263 @@
+package kr.or.ddit.basic;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import kr.or.ddit.util.DBUtil;
+
+/*
+	회원 관리를 하는 프로그램을 작성하시오.
+	( MYMEMBER 테이블 이용 )
+	
+	아래의 메뉴를 구성하고 각 메뉴의 기능을 구현하시오. (CRUD 기능 구현하기)
+	메뉴예시)
+	== 작업 선택 ==
+	 1. 자료 추가		==> C (insert문)
+	 2. 자료 삭제		==> D (delete문)
+	 3. 자료 수정		==> U (update문)
+	 4. 전체 자료 출력	==> R (select문)
+	 0. 작업 끝.
+	------------
+	
+	조건)
+	1) 자료 추가에서 '회원ID'는 중복되지 않는다. (중복되면 다시 입력 받는다.)
+	2) 자료 삭제는 '회원ID'를 입력 받아서 처리한다.
+	3) 자료 수정에서 '회원ID'는 변경되지 않는다.
+	
+*/
+public class JdbcTest06 {
+	private Scanner scan = new Scanner(System.in);
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
+	
+	public static void main(String[] args) {
+		new JdbcTest06().startMyMember();
+	}
+	
+	public void startMyMember() {
+		System.out.println("**********************");
+		System.out.println("      프 로 그 램 시 작");
+		System.out.println("**********************");
+		
+		while(true) {
+			int choice = displayMenu();
+			switch(choice) {
+			case 1:
+				insert();
+				break;
+			case 2:
+				delete();
+				break;
+			case 3:
+				update();
+				break;
+			case 4:
+				printAll();
+				break;
+			case 0:
+				System.out.println("작업을 종료합니다...");
+				System.exit(0);
+			default:
+				System.out.println("작업 번호 오류");
+			}
+		}
+	}
+	
+	// 1) 자료 추가에서 '회원ID'는 중복되지 않는다. (중복되면 다시 입력 받는다.)
+	private void insert() {
+		System.out.println(" -- 추가 작업 --");
+		try {
+			conn = DBUtil.getConnection();
+			
+			String id = null;
+			int count = 0;
+			
+			do {
+				System.out.print("회원 ID >> ");
+				id = scan.next();
+				
+				String sql1 = "select count(*) cnt from mymember "
+						+ "where mem_id = ?";
+				
+				pstmt = conn.prepareStatement(sql1);
+				pstmt.setString(1, id);
+				
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					count = rs.getInt("cnt");
+				}
+				
+				if(count > 0) {
+					System.out.println("회원 ID 중복");
+					System.out.println("다시 입력하세요...");
+					System.out.println();
+				}
+			} while(count > 0);
+			
+			System.out.print("비밀번호 >> ");
+			String pass = scan.next();
+			
+			System.out.print("회원이름 >> ");
+			String name = scan.next();
+			
+			System.out.print("전화번호 >> ");
+			String tel = scan.next();
+			scan.nextLine();
+			
+			System.out.print("주소 >> ");
+			String addr = scan.nextLine();
+			
+			String sql2 = "insert into mymember (mem_id, mem_pass, mem_name, mem_tel, mem_addr) "
+					+ "values(?, ?, ?, ?, ?)";
+			
+			pstmt = conn.prepareStatement(sql2);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pass);
+			pstmt.setString(3, name);
+			pstmt.setString(4, tel);
+			pstmt.setString(5, addr);
+			
+			int insertCnt = pstmt.executeUpdate();
+			if(insertCnt > 0) {
+				System.out.println("자료 추가 성공");
+			} else {
+				System.out.println("자료 추가 실패");
+			}
+			System.out.println();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) try { rs.close(); } catch(SQLException e) {}
+			if(pstmt!=null) try { pstmt.close(); } catch(SQLException e) {}
+			if(conn!=null) try { conn.close(); } catch(SQLException e) {}
+		}
+	}
+	
+	// 2) 자료 삭제는 '회원ID'를 입력 받아서 처리한다.
+	private void delete() {
+		System.out.println(" -- 삭제 작업 --");
+		System.out.print("삭제할 회원 ID >> ");
+		String id = scan.next();
+		
+		try {
+			conn = DBUtil.getConnection();
+			
+			String sql = "delete from mymember where mem_id = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			
+			int deleteCnt = pstmt.executeUpdate();
+			if(deleteCnt > 0) {
+				System.out.println("자료 삭제 성공");
+			} else {
+				System.out.println("자료 삭제 실패");
+			}
+			System.out.println();
+			
+		} catch (SQLException e) {
+			// TODO: handle exception
+		} finally {
+			if(rs!=null) try { rs.close(); } catch(SQLException e) {}
+			if(pstmt!=null) try { pstmt.close(); } catch(SQLException e) {}
+			if(conn!=null) try { conn.close(); } catch(SQLException e) {}
+		}
+	}
+	
+	// 3) 자료 수정에서 '회원ID'는 변경되지 않는다.
+	private void update() {
+		System.out.println(" -- 수정 작업 --");
+		System.out.print("회원 ID >> ");
+		String id = scan.next();
+		
+		System.out.print("수정할 비밀번호 >> ");
+		String pass = scan.next();
+		
+		System.out.print("수정할 회원이름 >> ");
+		String name = scan.next();
+		
+		System.out.print("수정할 전화번호 >> ");
+		String tel = scan.next();
+		scan.nextLine();
+		
+		System.out.print("수정할 주소 >> ");
+		String addr = scan.nextLine();
+		
+		try {
+			conn = DBUtil.getConnection();
+			
+			String sql = "update mymember set mem_pass = ?, mem_name = ?, mem_tel = ?, mem_addr = ? where mem_id = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, pass);
+			pstmt.setString(2, name);
+			pstmt.setString(3, tel);
+			pstmt.setString(4, addr);
+			pstmt.setString(5, id);
+			
+			int updateCnt = pstmt.executeUpdate();
+			if(updateCnt > 0) {
+				System.out.println("자료 수정 성공");
+			} else {
+				System.out.println("자료 수정 실패");
+			}
+			System.out.println();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) try { rs.close(); } catch(SQLException e) {}
+			if(pstmt!=null) try { pstmt.close(); } catch(SQLException e) {}
+			if(conn!=null) try { conn.close(); } catch(SQLException e) {}
+		}
+	}
+	
+	private void printAll() {
+		System.out.println(" -- 전체 출력 --");
+		try {
+			conn = DBUtil.getConnection();
+			
+			String sql = "select * from mymember";
+			
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				System.out.println("회원 ID : " + rs.getString("mem_id"));
+				System.out.println("비밀번호 : " + rs.getString("mem_pass"));
+				System.out.println("회원이름 : " + rs.getString("mem_name"));
+				System.out.println("전화번호 : " + rs.getString("mem_tel"));
+				System.out.println("주      소 : " + rs.getString("mem_addr"));
+				System.out.println("-------------------------------------");
+			}
+			System.out.println();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(rs!=null) try { rs.close(); } catch(SQLException e) {}
+			if(pstmt!=null) try { pstmt.close(); } catch(SQLException e) {}
+			if(conn!=null) try { conn.close(); } catch(SQLException e) {}
+		}
+	}
+	
+	private int displayMenu() {
+		System.out.println("== 작업 선택 ==");
+		System.out.println(" 1. 자료 추가");
+		System.out.println(" 2. 자료 삭제");
+		System.out.println(" 3. 자료 수정");
+		System.out.println(" 4. 전체 자료 출력");
+		System.out.println(" 0. 작업 끝.");
+		System.out.println("------------");
+		
+		System.out.print("메뉴 선택 >> ");
+		return scan.nextInt();
+	}
+
+}
